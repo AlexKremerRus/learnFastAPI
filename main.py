0667945c6acecb6d155b69fe99ae2c62b0aa2126
Alexey Nekhoroshev <nekhorosheff.a.v@yandex.ru>
2026-02-15 14:29:28 +0300

    работа с оффсет и лимит

--- main.py
+++ main.py
@@ -65,3 +65,21 @@ async def read_flight(from_code: str, to_code: str):
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
     return {"file": file_path}
+
+
+fake_tasks_db2 = [
+    {"task_name": "Task 1"},
+    {"task_name": "Task 2"},
+    {"task_name": "Task 3"},
+    {"task_name": "Task 4"},
+    {"task_name": "Task 5"},
+    {"task_name": "Task 6"},
+    {"task_name": "Task 7"},
+    {"task_name": "Task 8"},
+    {"task_name": "Task 9"},
+    {"task_name": "Task 10"},
+]
+
+@app.get("/tasks2")
+async def get_tasks(limit: int = 10, offset: int = 0):
+    return fake_tasks_db2[offset : offset + limit]
